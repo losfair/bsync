c60b094de79910c6aa35d27aec37c3f97e48371a
losfair <zhy20000919@hotmail.com>
2021-10-17 10:00:04 +0000

    CI

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ cd blkxmit
 rm -r ./dist || true
 mkdir dist
 cargo build --release --target x86_64-unknown-linux-musl
-cp ./target/x86_64-unknown-linux-musl/release/blkxmit ./dist/blkxmit.x86_64-unknown-linux-musl
+cp ../target/x86_64-unknown-linux-musl/release/blkxmit ./dist/blkxmit.x86_64-unknown-linux-musl
 cargo build --release --target aarch64-unknown-linux-musl
-cp ./target/aarch64-unknown-linux-musl/release/blkxmit ./dist/blkxmit.aarch64-unknown-linux-musl
+cp ../target/aarch64-unknown-linux-musl/release/blkxmit ./dist/blkxmit.aarch64-unknown-linux-musl
 llvm-strip ./dist/*
 cd ..
 
